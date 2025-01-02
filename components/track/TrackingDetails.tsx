c2898110302b992d238283da60fe47557d69a881
Osaretin Frank <osaretin.frank10@gmail.com>
2025-01-02 23:09:16 +0100

    add tracking Time line

--- components/track/TrackingDetails.tsx
+++ components/track/TrackingDetails.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Link from "next/link"
 import React from "react"
+import { GoCheckCircleFill } from "react-icons/go"
 
 export const TrackingDetails = () => {
   return (
@@ -18,7 +19,7 @@ export const TrackingDetails = () => {
 
       <div>
         <p className="md:text-2xl text-lg my-5 md:my-9">Order information</p>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
           <div className="space-y-4">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
               <p className="font-medium text-xs">Order Number:</p>
@@ -41,6 +42,52 @@ export const TrackingDetails = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
               <p className="font-medium text-xs">Status:</p>
               <p className="text-xs text-[#10617A]">In-Transit</p>
+            </div>
+          </div>
+          <div className="text-xs space-y-4">
+            <div className="grid grid-cols-[130px,auto] md:gap-[54px] gap-[27px] h-auto">
+              <div className="flex gap-[12px] h-[100%]">
+                <div className="flex flex-col items-center gap-[6px]">
+                  <GoCheckCircleFill className="text-[#37913B] text-3xl" />
+                  <div className="w-[2px] h-[100%] bg-[#37913B]" />
+                </div>
+                <div>
+                  <p>Nov 15, 2024</p>
+                  <p className="text-[11px] text-[#60605E] mt-[2px]">10:50</p>
+                </div>
+              </div>
+              <div>
+                <p>Delivery Request Created</p>
+                <div className="text-[11px] text-[#60605E] mt-[2px]">
+                  <p className="">Pickup: Ojota </p>
+                  <p>Payment: cash/card</p>
+                  <p>Amount: ₦2,500.00</p>
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-[130px,auto] md:gap-[54px] gap-[27px] h-auto">
+              <div className="flex gap-[12px] h-[100%]">
+                <div className="flex flex-col items-center gap-[6px]">
+                  <GoCheckCircleFill className="text-[#37913B] text-3xl" />
+                  <div className="w-[2px] h-[100%] bg-[#37913B]" />
+                </div>
+                <div>
+                  <p>Nov 15, 2024</p>
+                  <p className="text-[11px] text-[#60605E] mt-[2px]">10:50</p>
+                </div>
+              </div>
+              <div>
+                <p>Delivery Request Created</p>
+                <div className="text-[11px] text-[#60605E] mt-[2px]">
+                  <p className="">Pickup: Ojota </p>
+                  <p>Payment: cash/card</p>
+                  <p>Amount: ₦2,500.00</p>
+                  <p>
+                    Pickup address: 12 olayanju street, off ijesha-tedo. Lagos
+                    state
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
