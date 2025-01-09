1e4632ad350736fc60c897390dd9218ed60bb7ba
Osaretin Frank <osaretin.frank10@gmail.com>
2025-01-09 23:58:56 +0100

    add FAQs

--- components/faqs/Questions.tsx
+++ components/faqs/Questions.tsx
@@ -1,69 +1,56 @@
 "use client"
-import React from "react"
+import React, { useState } from "react"
 import { SearchIcon } from "../icons/Search"
 import { Tabs } from "../utilities/Tabs"
 import { Accordion } from "../utilities/Accordion"
-
-const items = [
-  {
-    title: "What is SwiftVia?",
-    content: (
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-        optio ducimus dolor quaerat sit, natus est odio cum sunt, accusantium
-        pariatur dignissimos maiores dolores provident placeat iusto non
-        perspiciatis magnam.
-      </div>
-    ),
-  },
-  {
-    title: "How can I register as a logistics partner with SwiftVia?",
-    content: (
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-        optio ducimus dolor quaerat sit, natus est odio cum sunt, accusantium
-        pariatur dignissimos maiores dolores provident placeat iusto non
-        perspiciatis magnam.
-      </div>
-    ),
-  },
-  {
-    title: "How does SwiftVia ensure package security during delivery?",
-    content: (
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-        optio ducimus dolor quaerat sit, natus est odio cum sunt, accusantium
-        pariatur dignissimos maiores dolores provident placeat iusto non
-        perspiciatis magnam. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Asperiores optio ducimus dolor quaerat sit, natus est odio cum
-        sunt, accusantium pariatur dignissimos maiores dolores provident placeat
-        iusto non perspiciatis magnam.
-      </div>
-    ),
-  },
-]
+import { faqs as all_faqs } from "@/constants/faqs"
 
 export const Questions = () => {
+  const [faqs, setFaqs] = useState<
+    {
+      title: string
+      content: React.ReactNode
+      type: "Rider" | "Customer" | "Logistics" | "General"
+    }[]
+  >(all_faqs)
+  const searchFAQ = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e?.target?.value?.toLowerCase()
+    console.log(value)
+    const filtered = all_faqs.filter((faq) => {
+      return (
+        faq.content.toLowerCase().includes(value) ||
+        faq.title.toLowerCase().includes(value)
+      )
+    })
+    setFaqs(filtered)
+    // console.log(filtered)
+  }
   const tabItems = [
     {
       title: "All",
-      children: <Accordion items={[...items, ...items, ...items]} />,
+      children: <Accordion items={faqs} />,
     },
     {
       title: "General",
-      children: <Accordion items={[...items, ...items]} />,
+      children: (
+        <Accordion items={faqs?.filter((faq) => faq.type === "General")} />
+      ),
     },
     {
       title: "Customer",
-      children: <Accordion items={[...items]} />,
+      children: <Accordion items={faqs} />,
     },
     {
       title: "Rider",
-      children: <Accordion items={[...items]} />,
+      children: (
+        <Accordion items={faqs?.filter((faq) => faq.type === "Rider")} />
+      ),
     },
     {
       title: "Logistic",
-      children: <Accordion items={[...items]} />,
+      children: (
+        <Accordion items={faqs?.filter((faq) => faq.type === "Logistics")} />
+      ),
     },
   ]
   return (
@@ -72,6 +59,7 @@ export const Questions = () => {
         <input
           className="w-full outline-none bg-[#F7F8F3] h-[48px] inline-block rounded-full px-[20px] md:pl-[75px] pl-[50px] py-[14px] text-[14px]"
           placeholder="Search for question"
+          onChange={searchFAQ}
         />
         <div className="absolute top-[50%] md:left-[40px] left-[25px] transform -translate-y-1/2">
           <SearchIcon />
---
+++ constants/faqs.ts
@@ -0,0 +1,83 @@
+export const faqs: {
+  title: string
+  content: string
+  type: "Rider" | "Customer" | "Logistics" | "General"
+}[] = [
+  {
+    type: "Rider",
+    title: "How do I onboard new riders on SwiftVia?",
+    content:
+      "To onboard new riders, navigate to the 'Riders' section in the dashboard. Click 'Add Rider' and enter the rider's full name and email address. The rider will receive an email with instructions to complete their registration. You can also upload multiple riders at once using a bulk list in the correct format.",
+  },
+  {
+    type: "Logistics",
+    title: "Can my company withdraw funds from the platform?",
+    content:
+      "Yes, your company can withdraw available funds from the riders' wallets. Riders can also request withdrawals, which you can approve in the 'Finance' section of the dashboard.",
+  },
+  {
+    type: "Logistics",
+    title: "How can my company become fully verified on SwiftVia?",
+    content: `To unlock full access to our platform, complete the verification process in the "Settings" section by submitting your company's CAC (Corporate Affairs Commission) documents and providing additional business information.`,
+  },
+  {
+    type: "Rider",
+    title: `Can my company track our riders in real-time?`,
+    content: `Yes, you can monitor all your riders in real-time via the "Riders" section on the dashboard, allowing you to keep track of their locations and delivery progress.`,
+  },
+  {
+    type: "Rider",
+    title: `Can I communicate with my riders through the platform?`,
+    content: `Absolutely! You can chat and call your riders directly through the dashboard at no extra cost, ensuring seamless communication.`,
+  },
+  {
+    type: "Rider",
+    title: `What should I do if my document verification fails?`,
+    content: `If your document verification fails, please contact our customer support team through the "Help & Support" page or send an email with your company details to support@swiftvia.com. We'll assist you in resolving the issue promptly.`,
+  },
+  {
+    type: "Rider",
+    title: `Will my riders be verified after being added?`,
+    content: `Yes, after being added to the platform, riders must complete a verification process by submitting their proof of identity and bike/vehicle details to become verified riders on SwiftVia.`,
+  },
+  {
+    type: "Rider",
+    title: `Can I block a rider from using SwiftVia services?`,
+    content: `Yes, you can block a rider from using our services. In the "Riders" section, click the three-dot menu next to the rider's name and select "Block Rider."`,
+  },
+  {
+    type: "General",
+    title: `Does SwiftVia charge a fee for using the dashboard?`,
+    content: `No, SwiftVia does not charge any fees for using the dashboard. It is a free tool designed to help grow your logistics company with our services. Feel free to refer other logistics company owners to our platform.`,
+  },
+  {
+    type: "General",
+    title: `Is SwiftVia a registered company?`,
+    content: `Yes, SwiftVia is a registered company in Nigeria under the Corporate Affairs Commission (CAC) as SwiftVia Logistics LTD.`,
+  },
+  {
+    type: "General",
+    title: `Can SwiftVia block my account?`,
+    content: `Yes, SwiftVia reserves the right to block your account if it is misused, if there is intent to cause harm, or if malicious activities are detected.`,
+  },
+  {
+    type: "Rider",
+    title: `Does SwiftVia earn from my rider's earnings?`,
+    content: `Yes, SwiftVia takes a small percentage from the delivery fee before the earnings are added to the rider's wallet. This helps support the growth and sustainability of our platform.`,
+  },
+  {
+    type: "Rider",
+    title: `Can my company use the SwiftVia Riders app?`,
+    content: `Yes, both riders and logistics companies can use the SwiftVia Riders app on mobile devices. However, the dashboard is only accessible to logistics companies via desktop.`,
+  },
+  {
+    type: "General",
+    title: `How do I submit a complaint?`,
+    content: `If you have a complaint, please email us at support@swiftvia.com. Our team will respond promptly to address your concerns.`,
+  },
+  {
+    type: "General",
+    title: `Is SwiftVia on social media?`,
+    content: `Yes, SwiftVia is active on various social media platforms. Follow us on Facebook (@swiftvia), Twitter (@swiftviaafrica), and Instagram (@swiftviaafrica) to stay updated on our latest news and services.`,
+  },
+]
