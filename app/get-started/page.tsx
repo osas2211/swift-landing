898aa3379b03c178a8bbd3483062bda9eb9c2fd7
Osaretin Frank <osaretin.frank10@gmail.com>
2025-01-12 15:46:18 +0100

    updates

--- app/get-started/page.tsx
+++ app/get-started/page.tsx
@@ -3,25 +3,18 @@ import { Header } from "@/components/Header"
 import { urls } from "@/constants/url"
 import React from "react"
 
+const images =
+  "https://res.cloudinary.com/ddr8rihqd/image/upload/v1714153594/logo/SwiftVia-AppStore_twhamt.png"
 export const metadata = {
-  title: {
-    default: "SwiftVia Logistics: Experience Fast Dispatch Delivery",
-    template: "%s | SwiftVia Logistics",
-  },
+  title: "Download App: Google & IOS App Stores",
   description:
-    "Find Dispatch Rider Nearby With SwiftVia Logistics, Grow Your Logistics Company, Earn More As A Rider, Live Tracking, Mobile Apps, Powerful Logistics Dashboard!",
-  twitter: {
-    card: "summary_large_image",
+    "SwiftVia App offers you the best delivery experience, tracking, active customer support, realiable riders, ratings and more.",
+  openGraph: {
+    images,
+    title: "Download SwiftVia App: Google & IOS App Stores",
+    description:
+      "SwiftVia App offers you the best delivery experience, tracking, active customer support, realiable riders, ratings and more.",
   },
-  metadataBase: `${urls.base_url}`,
-  alternates: {
-    canonical: "/",
-    languages: {
-      "en-US": "/en-US",
-    },
-  },
-  image:
-    "https://res.cloudinary.com/ddr8rihqd/image/upload/v1711286606/logo/dpitj4bpwpdsgb5q8uiw.png",
 }
 
 const GetStartedPage = () => {
